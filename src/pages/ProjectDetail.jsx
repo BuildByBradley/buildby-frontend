7419ce2c0796e6207337e96cb685670445efac11
Bradley Brits <ibradleyqbrits@gmail.com>
2025-07-16 22:31:46 +0200

    Add: ProjectDetail page component

---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectDetail = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Project Detail View</h1>
+      <p>This is where project-specific data will appear.</p>
+    </div>
+  );
+};
+
+export default ProjectDetail;
