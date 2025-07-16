6cb19d062e29115e86b6a2c744e7749f44f972d2
Bradley Brits <ibradleyqbrits@gmail.com>
2025-07-16 22:38:17 +0200

    Add: LogTime page

---
+++ src/pages/LogTime.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogTime = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Log Time</h1>
+      <p>This is where users can log hours worked on projects.</p>
+    </div>
+  );
+};
+
+export default LogTime;
